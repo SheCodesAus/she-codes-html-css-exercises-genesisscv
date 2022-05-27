b33a0ad36d545d40fc452fd6864ae48bbc6f90f4
genesisscv <genesisscv@gmail.com>
2022-05-28 09:35:42 +1000

    xx

--- keyboard/script.js
+++ keyboard/script.js
@@ -1,23 +1,32 @@
-window.onload = function() {
+window.onload = function () {
     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     // start
-
+    let keyboard = document.getElementById('keyboard');
     // end
 
     alphabet.forEach(letter => {
         // start
+        let keyButton = document.createElement('input');
+        keyButton.type = 'button';
+        keyButton.value = letter;
+        keyButton.classList.add('key');
+        keyButton.id = letter;
+        keyButton.addEventListener('click', event => {
+            highlightLetter(event.target.id)
+        })
+        keyboard.appendChild(keyButton);
 
         // end
     });
 }
 
-// start
+// start up to page 28
 
 // end
 
 
-function highlightLetter(letter){
+function highlightLetter(letter) {
     // start
-
+    console.log(letter)
     // end
 }
